fce7c48f6c8d25008a7dda580fc6e37061e31aa7
Tyler Smith <mail@tcry.pt>
2025-03-14 11:50:06 -0700

    refactor: Cleaner pattern for supervisor RPC metrics.

--- op-supervisor/supervisor/frontend/frontend.go
+++ op-supervisor/supervisor/frontend/frontend.go
@@ -2,6 +2,7 @@ package frontend
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
@@ -31,10 +32,7 @@ var _ sources.SupervisorQueryAPI = (*QueryFrontend)(nil)
 // CheckMessage checks the safety-level of an individual message.
 // The payloadHash references the hash of the message-payload of the message.
 func (q *QueryFrontend) CheckMessage(ctx context.Context, identifier types.Identifier, payloadHash common.Hash, executingDescriptor types.ExecutingDescriptor) (types.SafetyLevel, error) {
-	if q.Metrics != nil {
-		done := q.Metrics.RecordRPCServerRequest("supervisor_checkMessage")
-		defer done()
-	}
+	defer q.maybeRecordMetric("checkMessage")
 	return q.Supervisor.CheckMessage(ctx, identifier, payloadHash, executingDescriptor)
 }
 
@@ -45,10 +43,7 @@ func (q *QueryFrontend) CheckMessagesV2(
 	messages []types.Message,
 	minSafety types.SafetyLevel,
 	executingDescriptor types.ExecutingDescriptor) error {
-	if q.Metrics != nil {
-		done := q.Metrics.RecordRPCServerRequest("supervisor_checkMessagesV2")
-		defer done()
-	}
+	defer q.maybeRecordMetric("checkMessagesV2")
 	return q.Supervisor.CheckMessagesV2(ctx, messages, minSafety, executingDescriptor)
 }
 
@@ -59,85 +54,59 @@ func (q *QueryFrontend) CheckMessages(
 	ctx context.Context,
 	messages []types.Message,
 	minSafety types.SafetyLevel) error {
-	if q.Metrics != nil {
-		done := q.Metrics.RecordRPCServerRequest("supervisor_checkMessages")
-		defer done()
-	}
+	defer q.maybeRecordMetric("checkMessages")
 	return q.Supervisor.CheckMessages(ctx, messages, minSafety)
 }
 
 func (q *QueryFrontend) LocalUnsafe(ctx context.Context, chainID eth.ChainID) (eth.BlockID, error) {
-	if q.Metrics != nil {
-		done := q.Metrics.RecordRPCServerRequest("supervisor_localUnsafe")
-		defer done()
-	}
+	defer q.maybeRecordMetric("localUnsafe")
 	return q.Supervisor.LocalUnsafe(ctx, chainID)
 }
 
 func (q *QueryFrontend) CrossSafe(ctx context.Context, chainID eth.ChainID) (types.DerivedIDPair, error) {
-	if q.Metrics != nil {
-		done := q.Metrics.RecordRPCServerRequest("supervisor_crossSafe")
-		defer done()
-	}
+	defer q.maybeRecordMetric("crossSafe")
 	return q.Supervisor.CrossSafe(ctx, chainID)
 }
 
 func (q *QueryFrontend) Finalized(ctx context.Context, chainID eth.ChainID) (eth.BlockID, error) {
-	if q.Metrics != nil {
-		done := q.Metrics.RecordRPCServerRequest("supervisor_finalized")
-		defer done()
-	}
+	defer q.maybeRecordMetric("finalized")
 	return q.Supervisor.Finalized(ctx, chainID)
 }
 
 func (q *QueryFrontend) FinalizedL1(ctx context.Context) (eth.BlockRef, error) {
-	if q.Metrics != nil {
-		done := q.Metrics.RecordRPCServerRequest("supervisor_finalizedL1")
-		defer done()
-	}
+	defer q.maybeRecordMetric("finalizedL1")
 	return q.Supervisor.FinalizedL1(ctx)
 }
 
 // CrossDerivedFrom is deprecated, but remains for backwards compatibility to callers
 // it is equivalent to CrossDerivedToSource
 func (q *QueryFrontend) CrossDerivedFrom(ctx context.Context, chainID eth.ChainID, derived eth.BlockID) (derivedFrom eth.BlockRef, err error) {
-	if q.Metrics != nil {
-		done := q.Metrics.RecordRPCServerRequest("supervisor_crossDerivedFrom")
-		defer done()
-	}
+	defer q.maybeRecordMetric("crossDerivedFrom")
 	return q.Supervisor.CrossDerivedToSource(ctx, chainID, derived)
 }
 
 func (q *QueryFrontend) CrossDerivedToSource(ctx context.Context, chainID eth.ChainID, derived eth.BlockID) (derivedFrom eth.BlockRef, err error) {
-	if q.Metrics != nil {
-		done := q.Metrics.RecordRPCServerRequest("supervisor_crossDerivedToSource")
-		defer done()
-	}
+	defer q.maybeRecordMetric("crossDerivedToSource")
 	return q.Supervisor.CrossDerivedToSource(ctx, chainID, derived)
 }
 
 func (q *QueryFrontend) SuperRootAtTimestamp(ctx context.Context, timestamp hexutil.Uint64) (eth.SuperRootResponse, error) {
-	if q.Metrics != nil {
-		done := q.Metrics.RecordRPCServerRequest("supervisor_superRootAtTimestamp")
-		defer done()
-	}
+	defer q.maybeRecordMetric("superRootAtTimestamp")
 	return q.Supervisor.SuperRootAtTimestamp(ctx, timestamp)
 }
 
 func (q *QueryFrontend) AllSafeDerivedAt(ctx context.Context, derivedFrom eth.BlockID) (derived map[eth.ChainID]eth.BlockID, err error) {
-	if q.Metrics != nil {
-		done := q.Metrics.RecordRPCServerRequest("supervisor_allSafeDerivedAt")
-		defer done()
-	}
+	defer q.maybeRecordMetric("allSafeDerivedAt")
 	return q.Supervisor.AllSafeDerivedAt(ctx, derivedFrom)
 }
 
 func (q *QueryFrontend) SyncStatus(ctx context.Context) (eth.SupervisorSyncStatus, error) {
-	if q.Metrics != nil {
-		done := q.Metrics.RecordRPCServerRequest("supervisor_syncStatus")
-		defer done()
-	}
+	defer q.maybeRecordMetric("syncStatus")
 	return q.Supervisor.SyncStatus(ctx)
+}
+
+func (q *QueryFrontend) maybeRecordMetric(name string) func() {
+	return maybeRecordMetric(q.Metrics, "supervisor", name)
 }
 
 type AdminFrontend struct {
@@ -149,27 +118,32 @@ var _ sources.SupervisorAdminAPI = (*AdminFrontend)(nil)
 
 // Start starts the service, if it was previously stopped.
 func (a *AdminFrontend) Start(ctx context.Context) error {
-	if a.Metrics != nil {
-		done := a.Metrics.RecordRPCServerRequest("admin_start")
-		defer done()
-	}
+	defer a.maybeRecordMetric("start")
 	return a.Supervisor.Start(ctx)
 }
 
 // Stop stops the service, if it was previously started.
 func (a *AdminFrontend) Stop(ctx context.Context) error {
-	if a.Metrics != nil {
-		done := a.Metrics.RecordRPCServerRequest("admin_stop")
-		defer done()
-	}
+	defer a.maybeRecordMetric("stop")
 	return a.Supervisor.Stop(ctx)
 }
 
 // AddL2RPC adds a new L2 chain to the supervisor backend
 func (a *AdminFrontend) AddL2RPC(ctx context.Context, rpc string, jwtSecret eth.Bytes32) error {
-	if a.Metrics != nil {
-		done := a.Metrics.RecordRPCServerRequest("admin_addL2RPC")
-		defer done()
-	}
+	defer a.maybeRecordMetric("addL2RPC")
 	return a.Supervisor.AddL2RPC(ctx, rpc, jwtSecret)
+}
+
+func (a *AdminFrontend) maybeRecordMetric(name string) func() {
+	return maybeRecordMetric(a.Metrics, "admin", name)
+}
+
+// maybeRecordMetric emits metrics in the given namespace, only if we have an instance.
+// It returns a function to be invoked at the end of the method being recorded.
+func maybeRecordMetric(m FrontendMetrics, ns string, endpoint string) func() {
+	if m != nil {
+		method := fmt.Sprintf("%s_%s", ns, endpoint)
+		return m.RecordRPCServerRequest(method)
+	}
+	return func() {}
 }
